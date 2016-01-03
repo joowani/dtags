49ab26aa075d3b95c963d86c233c789d999ff63b
Joohwan Oh <joohwan.oh@outlook.com>
2016-01-03 01:28:54 -0500

    Add setup.cfg and edit setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="dtags",
-    description="Directory tags for the lazy programmers",
-    long_description=open('README.md').read(),
     version="1.0.0",
-    url="https://github.com/joowani/dtags",
+    description="Directory tags for lazy programmers",
+    long_description=open('README.md').read(),
     author="Joohwan Oh",
     author_email="joohwan.oh@outlook.com",
+    url="https://github.com/joowani/dtags",
+    download_url="https://github.com/joowani/dtags/tarball/1.0.0",
     packages=find_packages(),
     install_requires=["argcomplete"],
     entry_points={
