3146de7565f27190f912bae06d6566973084191f
Joohwan Oh <joohwan.oh@outlook.com>
2016-01-08 01:08:56 -0500

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="dtags",
-    version="1.0.0",
+    version="1.0.1",
     description="Directory tags for lazy programmers",
     long_description=open('README.md').read(),
     author="Joohwan Oh",
     author_email="joohwan.oh@outlook.com",
     url="https://github.com/joowani/dtags",
-    download_url="https://github.com/joowani/dtags/tarball/1.0.0",
+    download_url="https://github.com/joowani/dtags/tarball/1.0.1",
     packages=find_packages(),
     install_requires=["argcomplete"],
     entry_points={
